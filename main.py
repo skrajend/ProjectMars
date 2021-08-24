8d2ee3a52834804e70999b0f2df837a9d584ba7c
Senthil <rsenthilkumar.in@gmail.com>
2021-08-24 14:52:18 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+from flask import Flask
+app = Flask(__name__)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+@app.route('/')
+@app.route('/hello/')
+def hello_world():
+    return 'Hello World!\n'
 
+@app.route('/hello/<username>') # dynamic route
+def hello_user(username):
+    return 'Hello %s!\n' % username
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Hello from Mars')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(host='0.0.0.0')     # open for everyone
